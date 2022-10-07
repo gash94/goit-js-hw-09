341e5b22e301bfcf7b128137c37d153ea58d4a3e
gash94 <104354207+gash94@users.noreply.github.com>
2022-10-07 17:50:49 +0200

    update task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const bodyColor = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+let timerId = null;
+
+const randomBodyColor = {
+  DELAY: 1000,
+
+  getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  },
+
+  interval() {
+    timerId = setInterval(() => {
+      bodyColor.style.backgroundColor = this.getRandomHexColor();
+    }, this.DELAY);
+  },
+  start() {
+    btnStart.addEventListener('click', () => {
+      bodyColor.style.backgroundColor = this.getRandomHexColor();
+      this.interval();
+      btnStart.disabled = true;
+      btnStop.disabled = false;
+    });
+    btnStop.addEventListener('click', this.stop);
+  },
+
+  stop() {
+    clearInterval(timerId);
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+  },
+};
+
+randomBodyColor.start();
